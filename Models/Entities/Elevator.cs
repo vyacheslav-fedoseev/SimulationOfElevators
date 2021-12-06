42095f48712347af554e837fdb4d90ca30be5ce9
vyacheslav_fedoseev <slava140901@gmail.com>
2021-12-06 16:19:28 +0300

    realization of some Repositories and Enitites 2

--- Models/Entities/Elevator.cs
+++ Models/Entities/Elevator.cs
@@ -5,18 +5,26 @@ namespace Models.Entities
     public enum Direction { UP = 0, DOWN = 1, STOP = 2 }
     public class Elevator : PlaceInfo
     {
-        private int _currentFlor;
+        private int _currentFloor;
         private int _destinationFloor;
         private int _countPeople;
         public bool _isOpenDoor;
         private Direction _direction;
-        public void Move()
-        {
+        private float _maxAcceleration;
+        private float _maxSpeed;
+        private float _speed;
+        private float _startTime;
+        private int _maxCapacity;
 
-        }
-        public void Decide()
-        {
 
+
+        public Elevator(float maxAcceleration, float maxSpeed, int maxCapacity, int id)
+        {
+            _maxAcceleration = maxAcceleration;
+            _maxSpeed = maxSpeed;
+            _maxCapacity = maxCapacity;
+            _id = id;
         }
+
     }
 }
--- Models/Repositories/ElevatorRepository.cs
+++ Models/Repositories/ElevatorRepository.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models.Entities;
 
 namespace Models.Repositories
 {
-    public class ElevatorRepository
+    public class ElevatorRepository : IElevatorRepository
     {
+        private static List<Elevator> _elevator = new List<Elevator>();
+        private static int _id = 0;
+        private const int MAX_COUNT = 5;
+        public int Add(float maxAcceleration, float maxSpeed, int maxCapacity)
+        {
+            if (_id < MAX_COUNT)
+            {
+                _elevator.Add(new Elevator(maxAcceleration, maxSpeed, maxCapacity, _id));
+                return _id++;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public Elevator Find(int id)
+        {
+            return _elevator.Find(c => c._ID == id);
+        }
+        public IEnumerable<Elevator> GetAll()
+        {
+            return _elevator;
+        }
 
     }
 }
