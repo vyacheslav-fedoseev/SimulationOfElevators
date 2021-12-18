f507ae246cf7cc0914e6d4c94297f752e6e9eb28
vyacheslav_fedoseev <slava140901@gmail.com>
2021-12-18 09:22:08 +0300

    Finished: import/export EventsList

--- Models/Services/LoadEventsListService.cs
+++ Models/Services/LoadEventsListService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,24 @@ namespace Models.Services
     {
         public void Import(string importAddress)
         {
-            EventsListService._listOfEvents.Add(new EventItem(KindOfEvent.FireAlarm, 15f, 15f));
+            var strinfo = File.ReadAllLines(importAddress).ToList();
+
+            foreach (var el in strinfo.Select(lines => lines.Split(' ')))
+            {
+                if (el[0] == "0")
+                {
+                    EventsListService._listOfEvents.Add(new 
+                        EventItem(KindOfEvent.CreatePeople, int.Parse(el[1]), int.Parse(el[2]),
+                            int.Parse(el[3]), float.Parse(el[0])));
+                }
+                if (el[0] == "1")
+                {
+                    EventsListService._listOfEvents.Add(new 
+                        EventItem(KindOfEvent.FireAlarm, float.Parse(el[0]), float.Parse(el[1])));
+                }
+            }
+
+            
             EventsListService._listOfEvents.Add(new EventItem(KindOfEvent.CreatePeople, 13, 3,
                 10,
                 7f));
@@ -20,8 +38,25 @@ namespace Models.Services
 
         public void Export(string exportAddress)
         {
-            // EventsListService._listOfEvents.Add(new EventItem(KindOfEvent.CreatePeople, peopleCount, currentFloor, destinationFloor,
-            //    createPeopleTime));
+            string strInfo = null;
+            foreach (var element in EventsListService._listOfEvents)
+            {
+                if (element.KindOfEvent == KindOfEvent.CreatePeople)
+                {
+                    strInfo += $"{(int)element.KindOfEvent} " +
+                               $"{element.Parameters[0]} " +
+                               $"{element.Parameters[1]} " +
+                               $"{element.Parameters[2]} " +
+                               $"{element.TimeMarkers[0]}\n";
+                }
+                if (element.KindOfEvent == KindOfEvent.FireAlarm)
+                {
+                    strInfo += $"{(int)element.KindOfEvent} " +
+                               $"{element.TimeMarkers[0]} " +
+                               $"{element.TimeMarkers[1]}\n";
+                }
+            }
+            File.WriteAllText(exportAddress ?? "EventsList.txt", strInfo);
         }
     }
 }
